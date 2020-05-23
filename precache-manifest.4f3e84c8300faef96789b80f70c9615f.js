2111671841cb883a749857e55acd5b2ce0faf637
[jiansorge] <[ji@nsorge.com]>
2020-05-23 11:41:35 -0700

    Updates

---
+++ precache-manifest.4f3e84c8300faef96789b80f70c9615f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373820c0d870ba8d7dd30f458fba5f1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92b9f8bdb8979ef44569",
+    "url": "/static/css/main.c4cc0a09.chunk.css"
+  },
+  {
+    "revision": "a921b29ce4f22d7c6af9",
+    "url": "/static/js/2.69b268ce.chunk.js"
+  },
+  {
+    "revision": "92b9f8bdb8979ef44569",
+    "url": "/static/js/main.2dbb8eb1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
